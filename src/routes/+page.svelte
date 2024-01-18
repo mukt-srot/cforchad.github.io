34ae83d26f60b794ea577552c6ca2141f9414763
ashirbadsahu <ashirbadsahu300@gmail.com>
2024-01-18 18:53:23 +0530

    add sevlte-seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
   import Headertext from "$lib/layout-components/headertext.svelte";
   import Problemlist from "$lib/layout-components/problemlist.svelte";
+  import SvelteSeo from "svelte-seo";
 </script>
-
+<!---->
+<SvelteSeo title = "C for chad | Problems set site for chads"
+description = "C for chad is a problem set site for chads. It contains problems from classic books such as K&R, as well as challenges from popular online coding platforms like Leetcode and HackerRank."
+/>
 <Headertext />
 <Problemlist />
 
